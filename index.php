afa9814fb111238852e5b0922eb2a01787d7fb56
root <root@ubuntu.(none)>
2017-08-04 05:28:03 +0000

    这是注释

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-qweqweqwe
+
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+#
+# Committer: root <root@ubuntu.(none)>
+#
+# On branch master
+# Changes to be committed:
+#   (use "git reset HEAD <file>..." to unstage)
+#
+#	new file:   index.php
+#
